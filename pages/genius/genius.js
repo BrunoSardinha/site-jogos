b0333e098bd7f155f5346e35cf9045acca79c857
BrunoSardinha <bruno@sapoznik.com.br>
2022-03-14 19:43:16 -0300

    Start button + pre game sequence functioning

--- pages/genius/genius.js
+++ pages/genius/genius.js
@@ -1,22 +1,42 @@
 const buttonElements = document.querySelectorAll(".clickable")
-console.log(buttonElements)
+const startButton = document.getElementById("startButton")
+let randomSequence = []
 
-function setActive(element) {
-    element.classList.add("active")
+function setActive(element, activate) {
+    if(activate) {
+        element.classList.add("active")
+    }
+    else {
+        element.classList.remove("active")
+    }
 }
 
 function startGame() {
     console.log("startGame")
     document.getElementById("startButton").disabled = 'true';
+    buttonElements.forEach((element)=>{
+        setActive(element, false)
+    })
 }
 
 function initialPresentation () {
     let a = 1
     for(let element of buttonElements) {
         setTimeout (()=> {
-            setActive (element)
+            setActive (element, true)
         },1000*a)
         a=a+1
+    }
+    setTimeout(()=>{
+        startButton.innerHTML="COMEÇAR"
+        document.getElementById("startButton").disabled = false;
+    },5000)
+}
+
+function runSequence(){
+    const size = randomSequence.length
+    for(let counter = 0; counter<=size; counter++){
+
     }
 }
 
